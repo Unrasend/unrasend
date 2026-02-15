3d207e7e1b8e9aca6e6408b1045579582cd0931f
Vladyslav Luchkov <vladyslav.luchkov@planner5d.com>
2026-02-15 22:13:24 +0200

    build

--- src/App.routes.tsx
+++ src/App.routes.tsx
@@ -1,8 +1,8 @@
-import {createBrowserRouter, Navigate} from "react-router-dom";
+import {createHashRouter, Navigate} from "react-router-dom";
 import React from "react";
 import Home from './features/home/Home.tsx';
 
-export const AppRouter = createBrowserRouter([
+export const AppRouter = createHashRouter([
   {
     path: '/projects/:projectId',
     element: <Home />,
